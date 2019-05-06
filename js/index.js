abb29884e2516be9ec21e163930770ea1e9ed2a8
Pedrohrn <dev14.seucondominio@gmail.com>
2019-05-06 10:30:07 -0300

    finalizando js.

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 angular.module('passagem-servico').lazy
 
-.controller( 'PassagemServicos::IndexCtrl', [ '$scModal', 'scAlert', 'scToggle', function(scModal, scAlert, scToggle) {
+.controller( 'PassagemServicos::IndexCtrl', [ '$scModal', 'scAlert', 'scToggle', 'scTopMessages' , function(scModal, scAlert, scToggle, scTopMessages) {
 	var vm = this;
 
 	vm.listCtrl = {
@@ -11,18 +11,18 @@ angular.module('passagem-servico').lazy
 				pessoa_entrou: { id: 2, nome: 'Porteiro 2'},
 				data_cadastro: 30042018,
 				data_passagem: 30042018,
-				perfil: { id: 1, nome: 'Perfil 1' },
+				perfil: { id: 1, nome: 'Perfil Teste' },
 				objetos: [
 					{ categoria: { id: 1, label: 'Funcionamento' },
 						itens: [
-						 	{ qtd: 1, nome: 'teste' },
-						 	{ qtd: 13, nome: 'teste2' },
+						 	{ qtd: 1, label: 'teste' },
+						 	{ qtd: 13, label: 'teste2' },
 						],
 					},
 					{ categoria: { id: 2, label: 'Acontecimento' },
 						itens: [
-							{ qtd: 1, nome: 'teste32' },
-							{ qtd: 2, nome: 'teste223' }
+							{ qtd: 1, label: 'teste32' },
+							{ qtd: 2, label: 'teste223' }
 						]
 					}
 				],
@@ -31,6 +31,27 @@ angular.module('passagem-servico').lazy
 			}
 		]
 	};
+
+	vm.novaPassagemCtrl = {
+		new: false,
+
+		newRecord: function() {
+			if (this.new) {
+				scAlert.open({
+					title: 'Atenção!',
+					messages: [
+						{ msg: 'Tem certeza de que deseja fechar o formulário? Dados não salvos serão perdidos.'}
+					],
+					buttons: [
+						{ label: 'Sim', color: 'yellow', action: function() { vm.novaPassagemCtrl.new = false } },
+						{ label: 'Não', color: 'gray' }
+					]
+				})
+			} else {
+				this.new = true
+			}
+		},
+	}
 
 	vm.itemCtrl = {
 		init: function(passagem){
@@ -64,6 +85,47 @@ angular.module('passagem-servico').lazy
 		showFilter: function(){
 			this.filtroAvancado = !this.filtroAvancado
 		},
+	};
+
+	vm.categoriasCtrl = {
+		list: [
+			{ id: 1, label: 'Funcionamento', disabled: false },
+			{ id: 2, label: 'Acontecimento', disabled: false },
+			{ id: 3, label: 'Empréstimos', disabled: true },
+		],
+		edit: false,
+		novaCategoria: '',
+
+		desativarCategoria: function(categoria) {
+			title: '';
+			if (categoria.disabled) {
+				this.title = 'Deseja reativar a categoria?'
+			} else {
+				this.title = 'Deseja desativar a categoria?'
+			}
+			scAlert.open({
+				title: this.title,
+				buttons: [
+				  { label: 'Sim', color: 'yellow', action: function() { categoria.disabled = !categoria.disabled } },
+				  { label: 'Não', color: 'gray' }
+				]
+			})
+		},
+
+		editarCategoria: function(categoria) {
+			if (categoria.edit) {
+				scAlert.open({
+					title: 'Deseja cancelar a edição?',
+					buttons: [
+					  { label: 'Sim', color: 'yellow', action: function() { categoria.edit = false } },
+					  { label: 'Não', color: 'gray'}
+					]
+				})
+			} else {
+				categoria.edit = true
+				this.novaCategoria = angular.copy(categoria.label)
+			}
+		}
 	};
 
 	vm.perfilCtrl = {
@@ -143,6 +205,22 @@ angular.module('passagem-servico').lazy
 			} else {
 				perfil.edit.opened = true
 			}
+		},
+
+		desativar: function(perfil) {
+			title: '';
+			if (perfil.disabled) {
+				this.title = 'Deseja reativar o perfil?'
+			} else {
+				this.title = 'Deseja realmente desativar o perfil?'
+			}
+			scAlert.open({
+				title: this.title,
+				buttons: [
+					{ label: 'Sim', color: 'yellow', action: function() { perfil.disabled = !perfil.disabled } },
+					{ label: 'Não', color: 'gray' }
+				]
+			})
 		},
 
 		showCategorias: function() {
