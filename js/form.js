f0db8257c61a66af28118b5958464775ba10667f
Pedrohrn <dev14.seucondominio@gmail.com>
2019-05-07 14:39:12 -0300

    quase pronto. criada aba de permissões no modal de perfil. toggles agora funcionam da forma esperada, com avisos.

--- js/form.js
+++ js/form.js
@@ -1,5 +1,148 @@
 angular.module('passagem-servico').lazy
 
 .controller( 'PassagemServicos::FormCtrl', [ '$scModal', 'scAlert', 'scToggle', function(scModal, scAlert, scoggle) {
+	vm = this;
+	vm.params = { objetos: [] } || passagem;
+	vm.duplicata = null;
+
+	vm.init = {
+		init: function() {
+			if (passagem.edit.opened || vm.duplicata) {
+				vm.params = angular.copy(passagem)
+			} else {
+				vm.params = { perfil: [], objetos: [] }
+			}
+		}
+	};
+
+	vm.formCtrl = {
+		setPerfil: function() {
+			scAlert.open({
+				title: 'Atenção!',
+				messages: [
+					{ msg: 'O perfil de passagem foi alterado. O que deseja fazer com os objetos?'},
+					{ msg: 'Observação: Objetos sem categoria definida serão automaticamente excluídos.'}
+				],
+				buttons: [
+					{ label: 'Mesclar', tooltip: 'Mescla os itens, por categoria, do formulário abaixo com os itens do perfil selecionado', color: 'blue', action: function() {
+							if (Object.blank(vm.params.objetos)) {
+								console.log('1')
+									vm.params.perfil = angular.copy(vm.formCtrl.current_perfil)
+									vm.params.objetos = angular.copy(vm.params.perfil.objetos)
+							} else if ( vm.params.perfil && !Object.blank(vm.params.objetos) ) {
+								console.log('2')
+								vm.params.perfil = angular.copy(vm.formCtrl.current_perfil)
+								var menor = undefined
+								var maior = undefined
+								if (vm.params.objetos.length >= vm.params.perfil.objetos.length) {
+									maior = vm.params.objetos
+									menor = vm.params.perfil.objetos
+								} else {
+									menor = vm.params.objetos
+									maior = vm.params.perfil.objetos
+								}
+								for (var i = 0; i < maior.length; i++) {
+									for (var j = 0; j < menor.length; j++) {
+										if (vm.params.objetos[i].categoria.label == vm.params.perfil.objetos[j].categoria.label) {
+											vm.params.objetos[i].itens = vm.params.objetos[i].itens.concat(vm.params.perfil.objetos[j].itens)
+											vm.params.perfil.objetos.remove(vm.params.perfil.objetos[j])
+										}
+									}
+								}
+								vm.params.objetos = vm.params.objetos.concat(vm.params.perfil.objetos)
+							} else if (vm.params.objetos.length > 0 && Object.blank(vm.params.perfil)) {
+								console.log('3')
+								var aux = angular.copy(vm.params.objetos);
+								vm.params.objetos = [];
+								console.log(aux)
+								console.log(vm.params.objetos)
+								for (i = 0; i < aux.length; i ++) {
+									if (aux[i].categoria != null && aux[i].categoria != undefined && aux[i].categoria != {}) {
+										vm.params.objetos.push(aux[i])
+									}
+								}
+								console.log(aux)
+								console.log(vm.params.objetos)
+								vm.params.perfil = angular.copy(vm.formCtrl.current_perfil)
+								var menor = undefined
+								var maior = undefined
+								if (vm.params.objetos.length >= vm.params.perfil.objetos.length) {
+									maior = vm.params.objetos
+									menor = vm.params.perfil.objetos
+								} else {
+									menor = vm.params.objetos
+									maior = vm.params.perfil.objetos
+								}
+								for (var i = 0; i < maior.length; i++) {
+									for (var j = 0; j < menor.length; j++) {
+										if (vm.params.objetos[i].categoria.label == vm.params.perfil.objetos[j].categoria.label) {
+											vm.params.objetos[i].itens = vm.params.objetos[i].itens.concat(vm.params.perfil.objetos[j].itens)
+											vm.params.perfil.objetos.remove(vm.params.perfil.objetos[j])
+										}
+									}
+								}
+								vm.params.objetos = vm.params.objetos.concat(vm.params.perfil.objetos)
+							}
+						}
+					},
+					{ label: 'Sobreescrever', tooltip: 'Sobreescreve os objetos abaixo pelos objetos do perfil selecionado', color: 'yellow', action: function() {
+							vm.params.perfil = angular.copy(vm.formCtrl.current_perfil)
+							vm.params.objetos = angular.copy(vm.params.perfil.objetos)
+						},
+					},
+					{ label: 'Cancelar', color: 'gray', action: function() { scAlert.close() ; vm.params.perfil = [] } }
+				]
+			})
+		},
+
+		limparForm: function() {
+			scAlert.open({
+				title: 'Deseja realmente limpar o formulário abaixo?',
+				buttons: [
+					{ label: 'Sim', color: 'yellow', action: function() { vm.params.objetos = [] } },
+					{ label: 'Não', color: 'gray' }
+				]
+			})
+		},
+
+	};
+
+	vm.objetosCtrl = {
+		setCategoria: function(objeto) {
+			var count = 0;
+			for (var i = 0; i < vm.params.objetos.length; i ++) {
+				if (Object.blank(vm.params.objetos[i].categoria) || vm.params.objetos[i].categoria == undefined) {
+				} else if (vm.params.objetos[i].categoria.label == objeto.categoria.label) {
+					count++;
+				}
+			};
+			if (count >= 2) {
+				scAlert.open({
+					title: 'A categoria selecionada já existe na lista! Selecione outra!',
+					buttons: [
+						{ label: 'Ok', color: 'gray' }
+					]
+				})
+				objeto.categoria = {}
+				count = 1
+			}
+		},
+
+		addObjeto: function() {
+			vm.params.objetos.unshift({ categoria: undefined, itens: [], id: vm.params.objetos.length+1 })
+		},
+
+		rmvObjeto: function(objeto) {
+			vm.params.objetos.remove(objeto)
+		},
+
+		addItem: function(objeto){
+			objeto.itens.push({ qtd: undefined, label: '' })
+		},
+
+		rmvItem: function(objeto, item){
+			objeto.itens.remove(item)
+		},
+	};
 
 }])
--- js/index.js
+++ js/index.js
@@ -3,6 +3,16 @@ angular.module('passagem-servico').lazy
 .controller( 'PassagemServicos::IndexCtrl', [ '$scModal', 'scAlert', 'scToggle', 'scTopMessages' , function(scModal, scAlert, scToggle, scTopMessages) {
 	var vm = this;
 
+	vm.permissoesCtrl = {
+		list: [
+			{ id: 1, label: 'Editar itens', value: false },
+			{ id: 2, label: 'Editar categorias dos objetos', value: false },
+			{ id: 3, label: 'Editar itens e categorias dos objetos', value: false },
+			{ id: 4, label: 'Adicionar/Excluir itens', value: false },
+			{ id: 5, label: 'Adicionar/Excluir objetos', value: false },
+		],
+	};
+
 	vm.listCtrl = {
 		list: [
 			{ id: 1,
@@ -28,6 +38,16 @@ angular.module('passagem-servico').lazy
 				],
 				disabled: false,
 				observacoes: 'blabdsakdsabdsa',
+				notificacoes: [
+					{	status: { label: 'Enviada', color: 'yellow' },
+						user: { name: 'Pedro Henrique', email: 'pedrohrnogueira@outlook.com' },
+						data_envio: 02032018,
+					},
+					{ status: { label: 'Lida', color: 'green' },
+						user: { name: 'Pedro Henrique', email: 'pedrohrnogueira@outlook.com' },
+						data_leitura: 03032018,
+					},
+				],
 			}
 		]
 	};
@@ -54,21 +74,56 @@ angular.module('passagem-servico').lazy
 	}
 
 	vm.itemCtrl = {
+		duplicata: false,
+
 		init: function(passagem){
 			passagem.menu = new scToggle()
 			passagem.edit = new scToggle()
 			passagem.acc = new scToggle()
-			passagem.notificacoes = new scToggle()
 			passagem.modal = new scModal()
 		},
 
 		editar: function(passagem) {
-			if (passagem.edit.opened == false) {
-				passagem.edit.opened = true
-			} else {
-				passagem.edit.opened = false
+			if (passagem.edit && passagem.edit.opened && !passagem.acc.opened) {
+				scAlert.open({
+					title: 'Atenção!',
+					messages: [
+ 						{ msg: 'Deseja realmente fechar o formulário? Alterações não salvas serão perdidas.'}
+					],
+					buttons: [
+						{ label: 'Sim', color: 'yellow', action: function() { passagem.edit.close(); passagem.acc.open() } },
+						{ label: 'Não', color: 'gray'}
+					]
+				})
 			}
-		}
+			passagem.edit.open()
+		},
+
+		accToggle: function(passagem) {
+			if (passagem.edit && passagem.edit.opened) {
+				return vm.formCtrl.cancelar(passagem, function() { passagem.acc.toggle() })
+			} else {
+				passagem.acc.toggle()
+			}
+		},
+
+		duplicar: function(passagem) {
+			this.duplicata = true;
+		},
+
+		rmv: function(passagem) {
+			scAlert.open({
+				title: 'Atenção!',
+				messages: [
+					{ msg: 'Deseja realmente excluir este registro? Os dados não poderão ser recuperados após à exclusão.' }
+				],
+				buttons: [
+					{ label: 'Excluir', color: 'red', action: function() { vm.indexCtrl.listCtrl.list.splice(passagem.id-1, 1) } },
+					{ label: 'Cancelar', color: 'gray' }
+				]
+			})
+		},
+
 	};
 
 	vm.topToolBar = {
@@ -78,6 +133,25 @@ angular.module('passagem-servico').lazy
 			this.menuIsVisible = !this.menuIsVisible
 		},
 	};
+
+	vm.formCtrl = {
+		cancelar: function(passagem) {
+			if (vm.novaPassagemCtrl.new) {
+				return vm.novaPassagemCtrl.newRecord()
+			} else if (passagem.edit.opened) {
+				scAlert.open({
+					title: 'Atenção!',
+					messages: [
+						{ msg: 'Deseja realmente cancelar a edição? Os dados não salvos serão perdidos.'}
+					],
+					buttons: [
+						{ label: 'Sim', color: 'yellow', action: function() { passagem.edit.opened = false; passagem.acc.opened = true } },
+						{ label: 'Não', color: 'gray'}
+					]
+				})
+			}
+		}
+	}
 
 	vm.filtroCtrl = {
 		fitroAvancado: false,
@@ -125,7 +199,20 @@ angular.module('passagem-servico').lazy
 				categoria.edit = true
 				this.novaCategoria = angular.copy(categoria.label)
 			}
-		}
+		},
+
+		rmv: function(categoria) {
+			scAlert.open({
+				title: 'Atenção!',
+				messages: [
+					{ msg: 'Deseja realmente excluir essa categoria? As passagens antigas não serão afetadas, mas os perfis que usam essa categoria não a terão mais em seus objetos.' }
+				],
+				buttons: [
+					{ label: 'Excluir', color: 'red', action: function() { vm.categoriasCtrl.list.splice(categoria.id-1, 1) } },
+					{ label: 'Cancelar', color: 'gray'}
+				]
+			})
+		},
 	};
 
 	vm.perfilCtrl = {
@@ -146,7 +233,7 @@ angular.module('passagem-servico').lazy
 						]
 					}
 				],
-				disabled: true,
+				disabled: false,
 				permissoes: [
 					{ label: 'Adicionar/Excluir objetos e itens', checked: false },
 					{ label: 'Adicionar objetos e itens', checked: false },
@@ -161,14 +248,14 @@ angular.module('passagem-servico').lazy
 				objetos: [
 					{ categoria: { id: 1, label: 'Funcionamento'},
 						itens: [
-							{ qtd: 1, label: 'testessss' },
-							{ qtd: 32, label: 'uissss ui'}
+							{ qtd: 4, label: 'testessss' },
+							{ qtd: 33, label: 'uissss ui'}
 						]
 					},
 					{ categoria: { id: 2, label: 'Acontecimento'},
 						itens: [
-						  { qtd: 2, label: 'teste2sss' },
-						  { qtd: 3, label: 'testadase 234'}
+						  { qtd: 8, label: 'teste2sss' },
+						  { qtd: 9, label: 'testadase 234'}
 						]
 					}
 				],
@@ -186,6 +273,7 @@ angular.module('passagem-servico').lazy
 		modal: new scModal(),
 		viewPerfis: true,
 		viewCategorias: false,
+		viewPermissoes: false,
 
 		init: function(perfil) {
 			perfil.edit = new scToggle()
@@ -201,9 +289,26 @@ angular.module('passagem-servico').lazy
 
 		editar: function(perfil) {
 			if (perfil.edit.opened) {
-				return this.cancelar()
+				return vm.perfilCtrl.cancelar(perfil)
 			} else {
 				perfil.edit.opened = true
+			}
+		},
+
+		cancelar: function(perfil) {
+			if (perfil.edit.opened) {
+				scAlert.open({
+					title: 'Atenção!',
+					messages: [
+					  { msg: 'Deseja realmente fechar o formulário? Dados não salvos serão perdidos.'}
+					],
+					buttons: [
+						{ label: 'Sim', color: 'yellow', action: function() { perfil.edit.opened = false; } },
+						{ label: 'Não', color: 'gray'}
+					]
+				})
+			} else {
+				perfil.edit.toggle()
 			}
 		},
 
@@ -224,22 +329,26 @@ angular.module('passagem-servico').lazy
 		},
 
 		showCategorias: function() {
-			if (this.viewCategorias == false && this.viewPerfis == true) {
+			if (this.viewCategorias == false && this.viewPerfis == true || this.viewPermissoes == true) {
 				this.viewCategorias = true
 				this.viewPerfis = false
-			} else {
-				this.viewCategorias = true
+				this.viewPermissoes = false
+			}
+		},
+
+		showPermissoes: function() {
+			if (this.viewPermissoes == false && this.viewPerfis == true || this.viewCategorias == true) {
+				this.viewPermissoes = true
+				this.viewCategorias = false
 				this.viewPerfis = false
 			}
 		},
 
 		showPerfis: function() {
-			if (this.viewCategorias == true && this.viewPerfis == false) {
+			if (this.viewCategorias == true || this.viewPermissoes == true && this.viewPerfis == false) {
 				this.viewPerfis = true
 				this.viewCategorias = false
-			} else {
-				this.viewCategorias = false
-				this.viewPerfis = true
+				this.viewPermissoes = false
 			}
 		},
 	};
